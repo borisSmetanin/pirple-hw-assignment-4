8ba13da27e7b6e1fc6aae2ed1eb7b675cf905fa9
Boris Smetanin <boris.smetanin1703@gmail.com>
2019-03-01 10:18:09 +0200

    branch-2 Created the file model and the users controller

--- controllers/users.js
+++ controllers/users.js
@@ -3,16 +3,190 @@
  */
 let users = {};
 
-//'get_collection', 'get', 'post_collection', 'put', 'delete'
+/**
+ * Load dependencies
+ */
+
+let 
+    file_model = require('../lib/file_model'),
+    helpers    = require('../lib/helpers'),
+    crypto     = require('crypto');
+
+// This object contains all possible user fields and the their validation rules
+const user_fields_validation_rules = {
+
+    email: (email) => {
+
+        // This validation rule needs to be async since it check if user exists in an async way
+        return new Promise((resolve, reject) => {
+            let email_type = typeof email;
+            if (email_type == 'string') {
+
+               if (email.includes('.') && email.includes('@')) {
+
+                    // Make sure email is trimmed and lower case
+                   if (email.trim().toLowerCase().replace(/\s+/g, '') === email) {
+                       
+                        // File name is lower case email - saved as base64
+                       file_model.read('users', users.create_user_id(email), (err) => {
+                           // When creating new user we are expecting an error
+                           if (err) {
+                               resolve(false);
+                           } else {
+                               resolve('This email is taken');
+                           }
+                       });
+                   } else {
+                       resolve('Email must not contain any whit-space or upper case letters in it');
+                   }
+                   
+               } else {
+                   resolve(`Email is missing "." or "@"`);
+               }
+
+            } else {
+                resolve(`Wrong email type was given - ${email_type}`)
+            }
+        });
+
+    },
+    name: (name) => {
+        return users.string_field_validation(name, 'Name', 50);
+    },
+    city: (city) => {
+
+        return users.string_field_validation(city, 'City', 100);
+    },
+
+    street_name: (street_name) => {
+
+        return users.string_field_validation(street_name, 'Street Name', 100);
+    },
+
+    street_number: (street_number) => {
+
+        return users.string_field_validation(street_number, 'Street Number', 100);
+    },
+
+    password: (password) => {
+
+        let string_validation_error = users.string_field_validation(password, 'Password', 50);
+
+        if (string_validation_error) {
+            return string_validation_error;
+        }
+        
+        if (password.length < 8) {
+            return 'Password must be at least 8 characters long';
+        }
+
+        return false;
+    }
+}
+
+/**
+ * Validation helper for string field
+ * 
+ * @param {string} string_field_value 
+ * @param {string} string_field_name 
+ * @param {integer} max_chars_allowed 
+ * 
+ * @returns {string | boolean} - returns the string validation error or false if no error was found
+ */
+users.string_field_validation = (string_field_value, string_field_name, max_chars_allowed) => {
+    if ( ! string_field_value) {
+        return `${string_field_name} must not be empty`;
+    }
+
+    if (typeof string_field_value !== 'string') {
+        return `${string_field_name} must be string, "${typeof string_field_value}" was given.`;
+    }
+    
+    if (string_field_value.length > max_chars_allowed) {
+        return `${string_field_name} is too long. This field is restricted to ${max_chars_allowed} characters.`;
+    }
+
+    return false;
+} 
+
+/**
+ * Special Async function that loops on the given user payload, validate each field and returns a callback on the very first error it finds
+ * 
+ * @param {object} user_payload  || user's request payload
+ * @param {*} callback           || callback function will set error message of the validation if found, false for no validation errors
+ */
+users.validate = async (user_payload, callback) => {
+
+    let errors_found = false;
+    for (let user_field of Object.keys(user_payload)) {
+
+        // Will wait for each function to finish before moving to the next one
+        // This way i can use async functions in an "sync" way while the event loop is free to take other calls
+        let validation_error = await user_fields_validation_rules[user_field](user_payload[user_field]);
+
+        // Error was found - set the validation error and stop the loop
+        if ( ! errors_found && validation_error) {
+            errors_found = validation_error;
+            break;
+        }
+    }
+
+    callback(errors_found);
+}
+
+/**
+ * Create the user id
+ * 
+ * **This best be a functions as well since in the future we might want to change the id creation rules**
+ * @param   {string} email 
+ * @returns {string} user id (md5 string)
+ */
+users.create_user_id = (email) => {
+    return crypto.createHash('md5').update(email).digest('hex');
+}
+
+/**
+ * @param {string} password
+ * 
+ * @returns {string} hashed password string 
+ */
+users.hash_password = (password) => {
+
+    return crypto.createHash('sha256').update(password).digest('hex');
+}
+
 users.get = (payload, callback) => {
 
-    callback(
-        200, 
-        false,
-        {
-            msg: 'get'
-        }
-    );
+    /**
+     * GET /users/<email> 
+     */
+    let user_email = payload.id;
+
+    if ( user_email) {
+
+        file_model.read('users', users.create_user_id(user_email), (err, user_data) => {
+
+            if ( ! err && user_data) {
+
+                // Remove the password, ES6 style..
+                ({password, ...user_data} = user_data);
+                callback(200, false,  {
+                    message: `User's data was fetched successfully`,
+                    data: user_data
+                });
+            } else {
+                callback(404, true,  {
+                    message: `User was not found`
+                    
+                });
+            }
+        });
+    } else {
+        callback(412, true,  {
+            message: 'Need to specify the email in the URI'
+            
+        }) ;
+    }
 }
 
 users.get_collection = (payload, callback) => {
@@ -26,13 +200,69 @@ users.get_collection = (payload, callback) => {
     );
 }
 
-users.post_collection = (payload, callback) => {
+/**
+ * POST users
+ * 
+ * Create a single user
+ */
+users.post_collection = (request, callback) => {
 
-    callback(
-        200, 
-        false,
-        { msg: 'post_collection' }
-    );
+    // White-list the payload to prevent unwanted fields from been saved in the "DB" (user file..)
+    let 
+        mandatory_fields         = Object.keys(user_fields_validation_rules),
+        user_payload             = helpers.extract(request.payload, mandatory_fields),
+        user_payload_fields      = Object.keys(user_payload),
+        missing_mandatory_fields = mandatory_fields.filter(
+            mandatory_field => ! user_payload_fields.includes(mandatory_field)
+        );
+
+    // Make sure all mandatory fields are given
+    if ( ! missing_mandatory_fields.length > 0) {
+
+        // Validate each field in the request
+        users.validate(user_payload, (err) => {
+
+            if ( ! err) {
+                let 
+                    purified_email  = user_payload.email.trim().toLowerCase().replace(/\s+/g, ''),
+                    user_id         = users.create_user_id(purified_email),
+                    hashed_password = users.hash_password(user_payload.password);
+
+                // Set purified email (just in case since we already have validations check on this)
+                user_payload.email = purified_email;
+                // Sets hashed password - we dont want to save the real password
+                user_payload.password = hashed_password;
+                // Set the id for the user
+                user_payload.id    = user_id;
+
+                // Create the user
+                file_model.create('users', user_id, user_payload, (err, create_payload) => {
+
+                    if ( ! err) {
+                        callback( 200, false, { 
+                            message: 'User was created successfully',
+                            data: user_payload
+                        });
+                    } else {
+                        callback( 500, true, { 
+                            message: 'User was created successfully',
+                            data: create_payload
+                        });
+                    }
+                });
+
+            } else {
+                callback( 412, true,{ 
+                    message: `User registration filed with the following error: ${err}` 
+                });
+            }
+        });
+    } else {
+        // Missing Mandatory fields
+        callback( 412, true, { 
+            message: `User registration filed, missing the following mandatory fields: ${missing_mandatory_fields.join(', ')}` 
+        });
+    }
 }
 
 users.put = (payload, callback) => {
--- index.js
+++ index.js
@@ -1,4 +1,80 @@
 /**
+ *  ==== Technical notes =====
+ * 1. file system helper
+ * - create a file
+ * - update a file
+ * 
+ * 2. users service 
+ * * create new user - POST /user 
+ * ** fields - email (will be the key), name, city, street name, number, password. id will be md5 of the email
+ * ** need to make sure user does not exists before creating new one (try to load the given user's email)
+ * ** after creating the user - Create an order file with users is as the key - this will be a big JSON with all of its orders
+ * ** when user is created - the user orders JSON file is created as well as an empty JSON file
+ * 
+ * 
+ * 3. token service 
+ * - Create new token -  POST /token
+ * ** Need to provide email + password in the request (validation) 
+ * ** id will be the users id
+ * ** fields - email, id, expires (now + 1 hour)
+ * ** before creating user must provide email + password to check user exists
+ * ** token id must be random unique string - unique for each token !!
+ * ** token i valid for 1 hour
+ * 
+ * - extend existing token PUT /token/<id>
+ * - must provide email + password - this will add make token expired 1 hour from now
+ * 
+ * - logout - destroys a token DELETE /token/<id> 
+ * ** this will physically delete a token 
+ * 
+ * 4. login validation logic - special inner helper that will be provided with a token id + callback
+ * - needed for each action expect for POST /user 
+ *   GET /menu - NEEDS TO BE CHECKED AS WELL - according to the spec
+ * - user must provide token id
+ * - check if token exists
+ * - check if token not expired
+ * 
+ * 
+ * 5. continue users service
+ * 
+ * - show users data - GET user/<id>
+ * 
+ * - update users PUT /user/<id>
+ * ** update all but the email
+ * 
+ * - delete the user - DELETE /user/<id>
+ * **  this will physically delete a user
+ * - 
+ * 
+ * 7. get the possible menu: 
+ * - GET /menu - array of possible menu items: name, id, price, description
+ * 
+ * 8. orders service
+ * - create new order - POST /order 
+ * ** fields: 
+ * *** timestamp - will be the key
+ * *** item ids - must be real item ids  from the menu 
+ * *** user_id - from the user's data 
+ * *** payment_confirmed - false at first (maybe its a code?), 
+ * *** email_sent   - false at first
+ * *** 
+ * ** make the payment to strip - get some sort of payment id / confirmation data
+ * ** credit card will not be saved - simply passed to Strip API
+ * 
+ * ** save confirmation data in the order object
+ * ** once confirmation data is saved - send an email about it to the client
+ * ** save will be made if - one of the steps had failed (email/payment)
+ * ** 
+ * ** orders will be saved in the users order json, 
+ * ** this JSON will always be appended with a new data
+ * 
+ * - Show specific order - GET /order/<id>
+ * - Show all orders     - GET /order
+ *  
+ */
+
+
+/**
  * Load dependencies
  */
 
@@ -17,4 +93,3 @@ app.init = () => {
 
 // Start the application
 app.init();
-
