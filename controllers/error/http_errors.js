efbc1bc507158581aa2f3f76bb0c4bcb06720876
Boris Smetanin <boris.smetanin1703@gmail.com>
2019-08-02 13:42:26 +0300

    branch-9 Made errors beautiful again!

--- controllers/error/http_errors.js
+++ controllers/error/http_errors.js
@@ -6,6 +6,11 @@ let errors = {};
 // Export the controller at file start to avoid circular dependencies
 module.exports = errors;
 
+const helpers = require('../../lib/helpers');
+
+/**
+ * Basic Not found error handler for the API
+ */
 errors.not_found = (payload, callback) => {
     callback(
         404, 
@@ -13,13 +18,39 @@ errors.not_found = (payload, callback) => {
     );
 }
 
+/**
+ * Basic not found error handler for the UI
+ */
 errors.not_found_web = (payload, callback) => {
     callback(
         404, 
+        true,
         'Requested route was not found in this server',
-        '<h1>Requested route was not found in this server </h1>',
         'html'
     );
+}
+
+/**
+ * Special error web page generator to display beautified error pages
+ */
+errors.create_error_web_response = (payload) => {
+
+    return new Promise( (resolve, reject) => {
+        const home_page_data = {
+            title: payload.http_code,
+            error: payload.error,
+            user_is_logged: payload.user_is_logged
+        };
+
+        helpers.load_web_page('web_error_page', {}, home_page_data, (err, html_page) => {
+    
+            if ( ! err && html_page) {
+                resolve(html_page);
+            } else {
+                reject(err);
+            }
+        });
+    })
 }
 
  
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
 // TODO
-// 1. create users settings page
-// 2. show 500 / 403/ 404 error pages
 // 3. PWA - when order is complete - pop push notification
 // 4. PWA - use webworker to keep session alive by sending session refresh requests every 30 min after login
 // 5. make sure all errors are shown as 'toasts'
